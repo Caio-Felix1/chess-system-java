3e4dd3b9b1bb94a9e11b6b5450e478fc075a2bb0
Caio Felix <199407954+Caio-Felix1@users.noreply.github.com>
2025-07-28 23:13:47 -0300

    Special move - Castling

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
+	
+	private ChessMatch chessMatch;
 
-	public King(Board board, Color color) {
+	public King(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 	}
 	
 	@Override
@@ -19,6 +23,11 @@ public class King extends ChessPiece {
 	private boolean canMove(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p == null || p.getColor() != this.getColor();
+	}
+	
+	private boolean testRookCastling(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p != null && p instanceof Rook && p.getColor() == this.getColor() && p.getMoveCount() == 0;
 	}
 
 	@Override
@@ -39,10 +48,21 @@ public class King extends ChessPiece {
 			mat[p.getRow()][p.getColumn()] = true;
 		}	
 		
-		// verificando a esquerda da peça
+		// verificando à esquerda da peça
 		p.setValues(this.position.getRow(), this.position.getColumn() - 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
+			
+			// verificando se pode andar duas casas à direita (Special Move - Castling Queenside rook)
+			Position p1 = new Position(position.getRow(), position.getColumn() - 2);
+			Position p2 = new Position(position.getRow(), position.getColumn() - 3);
+			if (getMoveCount() == 0 && !chessMatch.getCheck() && !getBoard().thereIsAPiece(p1) && !getBoard().thereIsAPiece(p2)) {
+				Position postT = new Position(position.getRow(), position.getColumn() - 4);
+				if (testRookCastling(postT)) {
+					mat[p1.getRow()][p1.getColumn()] = true;
+				}
+				
+			}
 		}	
 		
 		// verificando a diagonal inferior esquerda da peça
@@ -67,6 +87,15 @@ public class King extends ChessPiece {
 		p.setValues(this.position.getRow(), this.position.getColumn() + 1);
 		if (getBoard().positionExists(p) && (!getBoard().thereIsAPiece(p) || this.isThereOpponentPiece(p))) {
 			mat[p.getRow()][p.getColumn()] = true;
+			
+			// verificando se pode andar duas casas à direita (Special Move - Castling Kingside rook)
+			Position p1 = new Position(position.getRow(), position.getColumn() + 2);
+			if (getMoveCount() == 0 && !chessMatch.getCheck() && !getBoard().thereIsAPiece(p1)) {
+				Position postT = new Position(position.getRow(), position.getColumn() + 3); 
+				if (testRookCastling(postT)) {
+					mat[p1.getRow()][p1.getColumn()] = true;
+				}
+			}
 		}
 		
 		// verificando a diagonal superior direita da peça
